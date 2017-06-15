31537528929f4649a9ebccc4782490724ae64103
Adrian Jutrowski <adrian.jutrowski@xstream.dk>
2017-06-15 12:45:28 +0200

    Updated webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,23 @@ module.exports = {
         server: "./server/index.js"
     },
     output: {
-        path: path.join(__dirname, "js"),
+        path: path.join(__dirname, "public"),
         filename: "[name]/bundle.js",
         chunkFilename: "[id].chunk.js"
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx?$/, loader: ['babel-loader', 'eslint-loader'], exclude: /node_modules/ },
+            { test: /\.html/, loader: 'file-loader?name=[path][name].[ext]', exclude: /node_modules/ },
         ]
     },
+    devServer: {
+        contentBase: path.join(__dirname, "public/client"),
+        compress: true,
+        port: 8081,
+        lazy: false,
+    },
+
     plugins: [
         new LiveReloadPlugin()
     ],
