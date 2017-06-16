d1faef67bf7ebc286072a5a05419d90972da6959
Adrian Jutrowski <adrian.jutrowski@xstream.dk>
2017-06-16 12:11:18 +0200

    Applied linter style suggestions

--- server/http.js
+++ server/http.js
@@ -4,29 +4,29 @@ import webpackMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
 import config from "../webpack.config.js";
 
-const app  = express();
+const app = express();
 const port = 8081;
 
 //For development:
 const compiler = webpack(config);
 const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
-    contentBase: 'client',
+    contentBase: "client",
     stats: {
         colors: true,
         hash: false,
         timings: true,
         chunks: false,
         chunkModules: false,
-        modules: false
-    }
+        modules: false,
+    },
 });
 
 app.use(middleware);
 app.use(webpackHotMiddleware(compiler));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(`${__dirname}../public`));
 
-app.listen(port, '0.0.0.0', (err) => {
+app.listen(port, "0.0.0.0", (err) => {
     if (err) {
         console.log(err);
     }
