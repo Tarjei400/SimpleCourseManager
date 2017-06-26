43cb311aec8d6264fe8efc8bc17c485de23a7dae
Adrian Jutrowski <adrian.jutrowski@xstream.dk>
2017-06-26 15:59:15 +0200

    Refactored environment configurations

--- server/EndpointConfig.js
+++
@@ -1,4 +0,0 @@
-export const APIUrl = "http://0.0.0.0:3000/api";
-export const AppHost = "http://0.0.0.0";
-export const AppPort = 8085;
-export const AppWebSocketUrl = `ws:/${AppHost}:${AppPort}`;
--- tests.config.js
+++
@@ -1,6 +0,0 @@
-//install asynchronous tests for jasmine
-var async = require('jasmine-es6/overrides/async');
-async.default();
-
-var testsContext = require.context('./client', true, /.(unit|integration|test)\.js$/);
-testsContext.keys().forEach(testsContext);
