647676daf02a35d09bdccb2107b99dcdc5158244
Adrian Jutrowski <adrian.jutrowski@xstream.dk>
2017-06-27 17:25:03 +0200

    Added apollo graphql server and graphiql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 import "./server/liveApi"
 import "./server/http"
+import "./server/gql/connector"
 import "./server/mockApiServer"
---
+++ server/gql/connector.js
@@ -0,0 +1,20 @@
+import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
+import bodyParser from "body-parser";
+import { server } from '../app';
+
+/***
+ *  Initialize graphQL api
+ */
+server.use("/graphql", bodyParser.json(), graphqlExpress({
+    schema : {}
+
+}));
+
+
+//TODO: Disabled this on product.
+/***
+ * Initialize graphiQL, UI for making gaphQL queries.
+ */
+server.use("/graphiql", bodyParser.json(), graphiqlExpress({
+    endpointURL: '/graphql'
+}));
